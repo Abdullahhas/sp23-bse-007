d01f20f9b371200a3e2c4f5fca4b257cb14c6b6c
abdullahhas <abdullah312003@gmail.com>
2024-11-26 21:06:02 +0500

    theory assignment 3

--- lab_assignment_03/server.js
+++ lab_assignment_03/server.js
@@ -1,6 +1,7 @@
 const express =  require('express')
+const expressLayouts = require('express-ejs-layouts');
 let app = express()
-
+app.use(expressLayouts);
 app.use(express.static('public'))
 
 app.set('view engine' , "ejs")
@@ -11,12 +12,12 @@ app.set('view engine' , "ejs")
 
 
 app.get("/home" , (req , res)=>{
-    res.render("index")
+    res.render("index"  , {cssFile : '/css/styles.css'} )
 })
 
 
 app.get("/contact" , (req , res)=>{
-    res.render("contact")
+    res.render("contact" , {cssFile: "/cv/style.css"})
 })
 
 app.listen (5000 , ()=>{
